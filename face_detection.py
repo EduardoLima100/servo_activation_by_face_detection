9d217267a51c378aeafeb1f84fe749fb30778f31
EduardoLima100 <eduardolima100@gmail.com>
2018-09-04 23:03:35 -0300

    post

---
+++ face_detection.py
@@ -0,0 +1,82 @@
+import cv2
+from pygame import mixer # Load the required library
+import os
+import serial
+
+conexao = serial.Serial('COM7', 9600)
+
+mixer.init()
+
+face_cascade = cv2.CascadeClassifier('opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
+profile_cascade = cv2.CascadeClassifier('opencv\sources\data\haarcascades\haarcascade_profileface.xml')
+
+cap = cv2.VideoCapture(0)
+
+positive = 0
+negative = 0
+ant = [0,0]
+while True:
+    
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    profiles = profile_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+    
+    profiles = profile_cascade.detectMultiScale(gray, 1.3, 5)
+    for (px,py,pw,ph) in profiles:
+        cv2.rectangle(img,(px,py),(px+pw,py+ph),(255,0,0),2)
+        roi_gray = gray[py:py+ph, px:px+pw]
+        roi_color = img[py:py+ph, px:px+pw]
+    
+    if len(faces)+len(profiles) > 0:
+        negative = 0
+        positive = positive + 1
+        
+        if positive > 10:
+            positive = 0
+            ant[0] = ant[1]
+            ant[1] = 1
+            
+            if ant[0] != ant[1]:
+                conexao.write(b'1')
+
+                print("Oi!!")
+                
+                mixer.music.load('ola.mp3')
+                mixer.music.play()
+                
+                
+
+    else:
+        positive = 0
+        negative = negative + 1
+        
+        if negative > 20:
+            negative = 0
+            ant[0] = ant[1]
+            ant[1] = 0
+            
+            if ant[0] != ant[1]:
+                conexao.write(b'0')
+                
+                print("Tchau!!")
+                           
+                mixer.music.load('tchau.mp3')
+                mixer.music.play()
+                #os.system("%systemroot%\system32\scrnsave.scr /s")
+            
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+conexao.write(b'0')
+
+cap.release()
+cv2.destroyAllWindows()
